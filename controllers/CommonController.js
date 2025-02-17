3f800aa0ecd800960b9d6f51c5f3eac68125c9b9
babluroy <bablu.roy@batchservice.com>
2025-02-18 02:40:38 +0530

    updated varible name

--- controllers/CommonController.js
+++ controllers/CommonController.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_KEY, region: process.env.AWS_REGION });
+AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_KEY, region: process.env.AWS_REGION_NAME });
 const s3 = new AWS.S3();
 
 /**
